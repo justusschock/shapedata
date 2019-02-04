a596794f9b6317a838fa8f871ab1772516736362
Justus Schock <12886177+justusschock@users.noreply.github.com>
2019-02-04 17:36:57 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,57 @@
 from setuptools import setup, find_packages
 curr_version = '0.1.0'
 
+import os
+import re
+
+def resolve_requirements(file):
+    requirements = []
+    with open(file) as f:
+        req = f.read().splitlines()
+        for r in req:
+            if r.startswith("-r"):
+                requirements += resolve_requirements(
+                    os.path.join(os.path.dirname(file), r.split(" ")[1]))
+            else:
+                requirements.append(r)
+    return requirements
+
+
+def read_file(file):
+    with open(file) as f:
+        content = f.read()
+    return content
+
+
+def find_version(file):
+    content = read_file(file)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", content,
+                              re.M)
+    if version_match:
+        return version_match.group(1)
+
+requirements = resolve_requirements(os.path.join(os.path.dirname(__file__),
+                                                 'requirements.txt'))
+
+readme = read_file(os.path.join(os.path.dirname(__file__), "README.md"))
+license = read_file(os.path.join(os.path.dirname(__file__), "LICENSE"))
+shapedata_version = find_version(os.path.join(os.path.dirname(__file__), "delira",
+                                           "__init__.py"))
+
 setup(
     name='shapedata',
-    version=curr_version,
+    version=shapedata_version,
     packages=find_packages(),
     url='https://github.com/justussschock/shapedata',
     license='MIT',
     author='Justus Schock',
     author_email='justus.schock@rwth-aachen.de',
     description='',
-    install_requires=["delira@git+https://github.com/justusschock/delira.git"],
-    python_requires="> 3.5",
+    test_suite="pytest",
+    long_description=readme,
+    license=license,
+    install_requires=requirements,
+    tests_require=["pytest-cov"],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
